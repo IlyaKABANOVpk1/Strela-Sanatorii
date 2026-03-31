e00631a469e19541998f404c04697bf9a22342f8
Илья Кабанов <tolatolkin@gmail.com>
2026-03-31 17:57:07 +0500

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Strela_Sanatorii
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
